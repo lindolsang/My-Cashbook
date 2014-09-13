b684f4859723ba887e3298355570035ab5bc6ff4
LinDol <lindolsang@nate.com>
2014-09-14 01:47:57 +0900

    I make to use database for loading log in MainAcitivity.

--- MyCashbook/src/kr/lindol/mycashbook/MainActivity.java
+++ MyCashbook/src/kr/lindol/mycashbook/MainActivity.java
@@ -1,12 +1,20 @@
 package kr.lindol.mycashbook;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import kr.lindol.mycashbook.db.CashLogContract.CashLogEntry;
+import kr.lindol.mycashbook.db.CashLogDbHelper;
+
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
@@ -16,6 +24,14 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * This class is Main Class in application
+ * 
+ * I refer http://entireboy.egloos.com/viewer/4152244
+ * 
+ * @author lindol
+ * 
+ */
 public class MainActivity extends Activity {
 
 	private static final int ADD_CASHLOG = 77;
@@ -27,16 +43,24 @@ public class MainActivity extends Activity {
 	private CustomList adapter;
 	private int testInputIndex = 0;
 
+	private CashLogDbHelper cashLogDbHelper = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// creating dbHelper
+		cashLogDbHelper = new CashLogDbHelper(getApplicationContext());
 
 		// today display to textview
 		TextView todayTextView = (TextView) findViewById(R.id.today);
 		Date today = new Date();
 		todayTextView.setText(String.format("%d-%d, %d", today.getDate(),
 				today.getMonth() + 1, (1900 + today.getYear())));
+
+		// read cashlog in cashLog Db
+		readCashLogInDb();
 
 		// do show cashlog
 		adapter = new CustomList(MainActivity.this, cashlogList);
@@ -69,7 +93,50 @@ public class MainActivity extends Activity {
 				startActivityForResult(intent, ADD_CASHLOG);
 			}
 		});
+
 	}
+
+	/**
+	 * This method is to read cashlog in Db. This method will fill cashLogList
+	 * from cashLog Db.
+	 * I refer http://stackoverflow.com/questions/4920528/iterate-through-rows-from-sqlite-query
+	 *   and http://developer.android.com/training/basics/data-storage/databases.html#DefineContract
+	 */
+	private void readCashLogInDb() {
+		SQLiteDatabase db = cashLogDbHelper.getReadableDatabase();
+
+		// Define a projection that specifies which columns from the database
+		// you will actually use after this query.
+		String[] projection = { CashLogEntry._ID,
+				CashLogEntry.COLUMN_NAME_ITEM_TITLE,
+				CashLogEntry.COLUMN_NAME_PRICE, };
+
+		// How you want the results sorted in the resulting Cursor
+		String sortOrder = CashLogEntry.COLUMN_NAME_MONTH_TAG + " DESC";
+
+		String selection = null;
+		String[] selectionArgs = null;
+
+		Cursor c = db.query(CashLogEntry.TABLE_NAME, // The table to query
+				projection, // The columns to return
+				selection, // The columns for the WHERE clause
+				selectionArgs, // The values for the WHERE clause
+				null, // don't group the rows
+				null, // don't filter by groups
+				sortOrder // The sort order
+				);
+
+		c.moveToFirst();
+		for (; c.isAfterLast() == false; c.moveToNext()) {
+			String tag = c
+					.getString(c
+							.getColumnIndexOrThrow(CashLogEntry.COLUMN_NAME_ITEM_TITLE));
+			long price = c.getLong(c
+					.getColumnIndexOrThrow(CashLogEntry.COLUMN_NAME_PRICE));
+
+			cashlogList.add(new CashLogItem(tag, (int) price));
+		}
+	};
 
 	/**
 	 * This method is compute Sum of cashlog and this method will update to
@@ -105,6 +172,9 @@ public class MainActivity extends Activity {
 			for (CashLogItem cashLog : itemList) {
 				adapter.add(new CashLogItem(cashLog.getTag(), cashLog
 						.getPrice()));
+				insertCashLogIntoDb(
+						new CashLogItem(cashLog.getTag(), cashLog.getPrice()),
+						new Date());
 			}
 
 			// compute sum of cash and update sum's textView
@@ -117,5 +187,37 @@ public class MainActivity extends Activity {
 							itemCount), Toast.LENGTH_SHORT).show();
 
 		}
+	}
+
+	/**
+	 * insert new row into DB for cashLog
+	 * 
+	 * @param cashLogItem
+	 * @param date
+	 */
+	private void insertCashLogIntoDb(CashLogItem cashLogItem, Date date) {
+
+		// Gets the data repository in write mode
+		SQLiteDatabase db = cashLogDbHelper.getWritableDatabase();
+
+		SimpleDateFormat monthTagFormat = new SimpleDateFormat("yyyyMM");
+		SimpleDateFormat dayTagFormat = new SimpleDateFormat("yyyyMMdd");
+
+		// Create a new map of values, where column names are the keys
+		ContentValues values = new ContentValues();
+		values.put(CashLogEntry.COLUMN_NAME_EVENT_ID,
+				(int) (Math.random() * 10000));
+		values.put(CashLogEntry.COLUMN_NAME_MONTH_TAG,
+				monthTagFormat.format(date));
+		values.put(CashLogEntry.COLUMN_NAME_DAY_TAG, dayTagFormat.format(date));
+		values.put(CashLogEntry.COLUMN_NAME_ITEM_TITLE, cashLogItem.getTag());
+		values.put(CashLogEntry.COLUMN_NAME_PRICE, cashLogItem.getPrice());
+
+		// Insert the new row, returning the primary key value of the new row
+		long newRowId;
+		newRowId = db.insert(CashLogEntry.TABLE_NAME,
+				CashLogEntry.COLUMN_NAME_NULLABLE, values);
+
+		Log.d("MyCashbook", String.format("newRowId: %d", newRowId));
 	}
 }
---
+++ MyCashbook/src/kr/lindol/mycashbook/db/CashLogDbHelper.java
@@ -0,0 +1,56 @@
+package kr.lindol.mycashbook.db;
+
+import java.util.Date;
+
+import kr.lindol.mycashbook.CashLogItem;
+import kr.lindol.mycashbook.db.CashLogContract.CashLogEntry;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * This class is Helper class for using cashLog DB. I refer
+ * http://developer.android.com/training/basics/data-storage/databases.html and
+ * http://www.sqlite.org/datatype3.html
+ * 
+ * @author lindol
+ */
+public class CashLogDbHelper extends SQLiteOpenHelper {
+
+	public static final int DATABASE_VERSION = 1;
+	public static final String DATABASE_NAME = "cashLog.db";
+
+	private static final String TEXT_TYPE = " TEXT";
+	private static final String COMMA_SEP = ",";
+	private static final String SQL_CREATE_ENTRIES = "CREATE TABLE "
+			+ CashLogEntry.TABLE_NAME + " ( " + CashLogEntry._ID
+			+ " INTEGER PRIMARY KEY, " + CashLogEntry.COLUMN_NAME_EVENT_ID
+			+ TEXT_TYPE + COMMA_SEP + CashLogEntry.COLUMN_NAME_MONTH_TAG
+			+ TEXT_TYPE + COMMA_SEP + CashLogEntry.COLUMN_NAME_DAY_TAG
+			+ TEXT_TYPE + COMMA_SEP + CashLogEntry.COLUMN_NAME_ITEM_TITLE
+			+ TEXT_TYPE + COMMA_SEP + CashLogEntry.COLUMN_NAME_PRICE
+			+ TEXT_TYPE + " )";
+
+	private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS "
+			+ CashLogEntry.TABLE_NAME;
+
+	public CashLogDbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(SQL_CREATE_ENTRIES);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(SQL_DELETE_ENTRIES);
+		onCreate(db);
+	}
+
+	@Override
+	public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		onUpgrade(db, oldVersion, newVersion);
+	}
+}
