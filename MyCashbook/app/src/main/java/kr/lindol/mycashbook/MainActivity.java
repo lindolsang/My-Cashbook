ac56a5d6845581781816ea3688463b21211efc87
Juyeon <lindol@Juyeonui-MacBookPro.local>
2021-12-09 17:01:20 +0900

    Implement splash screen

--- MyCashbook/app/src/main/java/kr/lindol/mycashbook/MainActivity.java
+++ MyCashbook/app/src/main/java/kr/lindol/mycashbook/MainActivity.java
@@ -2,7 +2,7 @@ package kr.lindol.mycashbook;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,14 +10,23 @@ import kr.lindol.mycashbook.list.CashLogListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int DELAY_MILLIS = 250;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onButton1_clicked(View view) {
-        Intent i = new Intent(this, CashLogListActivity.class);
-        startActivity(i);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), CashLogListActivity.class));
+                finish();
+            }
+        }, DELAY_MILLIS);
     }
 }
